6be5ff7423d1c4dbe7058977ac9e75ef2ffac75b
yuyenews <yuyemail@163.com>
2021-07-16 12:13:22 +0800

    修改细节

--- src/main/java/com/martian/starter/handler/DispatcherHandler.java
+++ src/main/java/com/martian/starter/handler/DispatcherHandler.java
@@ -6,13 +6,14 @@ import com.martian.config.MartianConfig;
 import com.martian.config.model.CrossDomainConfig;
 import io.magician.common.annotation.TCPHandler;
 import io.magician.tcp.codec.impl.http.request.MagicianRequest;
+import io.magician.tcp.codec.impl.http.request.MagicianResponse;
 import io.magician.tcp.handler.TCPBaseHandler;
 
 /**
  * 核心控制器
  */
 @TCPHandler(path = "/")
-public class CoreHandler implements TCPBaseHandler<MagicianRequest> {
+public class DispatcherHandler implements TCPBaseHandler<MagicianRequest> {
 
     /**
      * 配置
@@ -24,12 +25,13 @@ public class CoreHandler implements TCPBaseHandler<MagicianRequest> {
     public void request(MagicianRequest magicianRequest) {
 
         CrossDomainConfig crossDomainConfig = martianConfig.crossDomainConfig();
+        MagicianResponse response = magicianRequest.getResponse();
 
-        magicianRequest.getResponse().setResponseHeader("Access-Control-Allow-Origin", crossDomainConfig.getOrigin());
-        magicianRequest.getResponse().setResponseHeader("Access-Control-Allow-Methods", crossDomainConfig.getMethods());
-        magicianRequest.getResponse().setResponseHeader("Access-Control-Max-Age", crossDomainConfig.getMaxAge());
-        magicianRequest.getResponse().setResponseHeader("Access-Control-Allow-Headers", crossDomainConfig.getHeaders());
-        magicianRequest.getResponse().setResponseHeader("Access-Control-Allow-Credentials", crossDomainConfig.getCredentials());
+        response.setResponseHeader("Access-Control-Allow-Origin", crossDomainConfig.getOrigin());
+        response.setResponseHeader("Access-Control-Allow-Methods", crossDomainConfig.getMethods());
+        response.setResponseHeader("Access-Control-Max-Age", crossDomainConfig.getMaxAge());
+        response.setResponseHeader("Access-Control-Allow-Headers", crossDomainConfig.getHeaders());
+        response.setResponseHeader("Access-Control-Allow-Credentials", crossDomainConfig.getCredentials());
 
         MagicianWeb.createWeb().request(magicianRequest);
     }
